be213732dd1ccfdef5d75ce0e0d4bceef4c8d4e2
Nicole Abramowski <nabramow@gmail.com>
2025-01-11 13:43:12 -0800

    Sort imports

--- app/web/jest.config.ts
+++ app/web/jest.config.ts
@@ -1,5 +1,5 @@
-import nextJest from "next/jest";
 import type { Config } from "jest";
+import nextJest from "next/jest";
 
 // Providing the path to your Next.js app which will enable loading next.config.js and .env files
 const createJestConfig = nextJest({ dir: "./" });
